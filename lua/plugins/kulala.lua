84beff9f12ef9564bf8a0e922a3e255f24d3a257
Anthony Lyons <rampantspark@gmail.com>
2025-01-15 12:24:17 -0600

    Fixed mappings

--- lua/plugins/kulala.lua
+++ lua/plugins/kulala.lua
@@ -8,55 +8,55 @@ return {
       vim.api.nvim_set_keymap(
         "n",
         "<leader>kr",
-        "require('kulala').run()<CR>",
+        ":lua require('kulala').run()<CR>",
         { noremap = true, silent = true, desc = "Send request" }
       )
       vim.api.nvim_set_keymap(
         "n",
         "<leader>kR",
-        "require('kulala').replay()<CR>",
+        ":lua require('kulala').replay()<CR>",
         { noremap = true, silent = true, desc = "Replay last request" }
       )
       vim.api.nvim_set_keymap(
         "n",
         "<leader>ks",
-        "require('kulala').scratchpad()<CR>",
+        ":lua require('kulala').scratchpad()<CR>",
         { noremap = true, silent = true, desc = "Open scratchpad" }
       )
       vim.api.nvim_set_keymap(
         "n",
         "<leader>kc",
-        "require('kulala').copy()<CR>",
+        ":lua require('kulala').copy()<CR>",
         { noremap = true, silent = true, desc = "Copy request as cURL command to clipboard" }
       )
       vim.api.nvim_set_keymap(
         "n",
         "<leader>kC",
-        "require('kulala').close()<CR>",
+        ":lua require('kulala').close()<CR>",
         { noremap = true, silent = true, desc = "Close kulala window and current buffer" }
       )
       vim.api.nvim_set_keymap(
         "n",
         "<leader>kt",
-        "require('kulala').toggle_view()<CR>",
+        ":lua require('kulala').toggle_view()<CR>",
         { noremap = true, silent = true, desc = "Toggle between header and body view" }
       )
       vim.api.nvim_set_keymap(
         "n",
         "<leader>kS",
-        "require('kulala').search()",
+        ":lua require('kulala').search()",
         { noremap = true, silent = true, desc = "Search for all .http and .rest files in cwd" }
       )
       vim.api.nvim_set_keymap(
         "n",
         "<leader>kp",
-        "require('kulala').jump_prev()",
+        ":lua require('kulala').jump_prev()",
         { noremap = true, silent = true, desc = "Jump to previous request" }
       )
       vim.api.nvim_set_keymap(
         "n",
         "<leader>kn",
-        "require('kulala).jump_next()",
+        ":lua require('kulala).jump_next()",
         { noremap = true, silent = true, desc = "Jump to next request" }
       )
       vim.api.nvim_set_keymap(
